6e3b3770bbbce9336f4d5196776d84a83e462e34
aryna.solodenko <aryna.karali@itera.com>
2024-05-17 17:29:56 +0300

    feat: change login click routing

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@
 
       <AppLink to="/about" class="mr-4">Contact us</AppLink>
 
-      <AppLink to="" v-if="!isAuth" @click="login">Login</AppLink>
+      <AppLink to="/login" v-if="!isAuth">Login</AppLink>
       <v-btn icon="mdi-account" v-if="isAuth" to="/account">
         <v-avatar
           size="36px"
@@ -26,22 +26,17 @@
             color="white"
           ></v-icon>
         </v-avatar>
-        <div v-if="isAdmin">ADMIN</div>
+        <!-- <div v-if="isAdmin">ADMIN</div> -->
       </v-btn>
     </template>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  import { handleSignIn } from '@/firebase';
   import { useUserStore } from '@/store/user';
   import AppLink from '@/components/AppLink.vue'
   import { storeToRefs } from 'pinia';
 
   const userStore = useUserStore();
   const { user, isAdmin, isAuth } = storeToRefs(userStore);
-
-  const login = () => {
-    handleSignIn();
-  }
 </script>
