435e06a19ecdabffbec742c05f586695a96a5b81
aryna.solodenko <aryna.karali@itera.com>
2025-10-31 13:35:31 +0200

    refactor use consts

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const APPROVED = 'approved';
 export const REJECTED = 'rejected';
 export const FULFILLED = 'fulfilled';
 export const UNFULFILLED = 'unfulfilled';
+export const VISIT = 'visit';
+export const ADOPT = 'adopt';
